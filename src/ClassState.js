787df72f611d661694607720ea9b2820ad1bd103
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-08-31 11:47:39 -0300

    estados compuestos imperativos

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Loading } from "./Loading.js";
 
+const SECURITY_CODE = "paradigma";
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      error: true,
+      value: "",
+      error: false,
       loading: false,
     };
   }
@@ -26,19 +29,33 @@ class ClassState extends React.Component {
     if (!!this.state.loading) {
       setTimeout(() => {
         console.log("comenzando la validacion");
-        this.setState({loading: false});
+
+        if (this.state.value === SECURITY_CODE) {
+          this.setState({
+            loading: false,
+            error: false
+          });
+        } else {
+          this.setState({
+            loading: false,
+            error: true,
+          });
+        }
+
         console.log("terminando la validacion");
       }, 3000);
     }
   }
 
   render() {
+    // const {error, loading, value} = this.state;
+
     return(
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor, escribe el código de seguridad.</p>
 
-        {this.state.error && (
+        {(this.state.error && !this.state.loading) && (
           <p>Error: el código es incorrecto</p>
         )}
 
@@ -46,7 +63,15 @@ class ClassState extends React.Component {
           <Loading />
         )}
 
-        <input placeholder="Código de seguridad" />
+        <input
+          placeholder="Código de seguridad"
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({
+              value: event.target.value,
+            })
+          }}
+          />
         <button
           onClick={() => this.setState({ loading: true })}
         >Comprobar</button>
