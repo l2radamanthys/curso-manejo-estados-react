57326505fbe05e5c103071f8f0ce69bd7ab6d977
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-08-31 12:22:56 -0300

    estados inperactivos con useState

--- src/UseState.js
+++ src/UseState.js
@@ -7,12 +7,11 @@ function UseState({ name }) {
     value: '',
     error: false,
     loading: false,
+    deleted: false,
+    confirmed: false
   });
-  const [value, setValue] = React.useState("");
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
 
-  console.log(state)
+  console.log(":::", state)
 
   React.useEffect(() => {
     console.log("empezando el efecto");
@@ -25,6 +24,7 @@ function UseState({ name }) {
             ...state,
             loading: false,
             error: false,
+            confirmed: true,
           });
         } else {
           setState({
@@ -39,9 +39,10 @@ function UseState({ name }) {
     }
 
     console.log("terminando.");
-  }, [loading, state.loading]);
+  }, [state.loading]);
 
-  return (
+  if (!state.deleted && !state.confirmed) {
+    return (
       <div>
         <h2>Eliminar {name}</h2>
         <p>Por favor, escribe el código de seguridad.</p>
@@ -72,7 +73,49 @@ function UseState({ name }) {
           }}
         >Comprobar</button>
       </div>
-  )
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <React.Fragment>
+        <h2>Eliminar {name}</h2>
+        <p>Pedimos confirmación</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              value: "",
+              deleted: true,
+            });
+          }}
+        >Sí, eliminar</button>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              value: "",
+            });
+          }}
+        >No</button>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <h2>Eliminar {name}</h2>
+        <p>Eliminado con exíto.</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              deleted: false,
+            });
+          }}
+        >Resetear</button>
+      </React.Fragment>
+    );
+  }
 }
 
 export { UseState };
