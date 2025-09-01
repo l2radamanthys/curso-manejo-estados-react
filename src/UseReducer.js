a11fb9c4b102c2dae1311c092706d604f582278c
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-08-31 22:27:25 -0300

    actionCreators y actionTypes

--- src/UseReducer.js
+++ src/UseReducer.js
@@ -4,30 +4,14 @@ const SECURITY_CODE = "paradigma";
 
 function UseReducer({ name }) {
   const [state, dispatch] = React.useReducer(reducer, initialState);
-
-  const onConfirm = () => {
-    dispatch({ type: actionTypes.confirm });
-  };
-
-  const onError = () => {
-    dispatch({ type: actionTypes.error });
-  };
-
+  const onConfirm = dispatch({ type: actionTypes.confirm });
+  const onError = dispatch({ type: actionTypes.error });
+  const onCheck = dispatch({ type: actionTypes.check });
+  const onDelete = dispatch({ type: actionTypes.delete });
+  const onReset = dispatch({ type: actionTypes.reset });
   const onWrite = ({ target: { value } }) => {
     dispatch({ type: actionTypes.write, payload: value });
   };
-
-  const onCheck = () => {
-    dispatch({ type: actionTypes.check });
-  };
-
-  const onDelete = () => {
-    dispatch({ type: actionTypes.delete });
-  };
-
-  const onReset = () => {
-    dispatch({ type: actionTypes.reset });
-  }
 
   React.useEffect(() => {
     console.log("empezando el efecto");
